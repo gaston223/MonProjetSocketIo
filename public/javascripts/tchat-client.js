26a6c4447259ff016967d0ca3f67c0f03b026db4
gaston223 <g.coulibalyy@gmail.com>
2018-06-04 15:15:28 +0200

    Tchat mis en place coté serveur et client

--- public/javascripts/tchat-client.js
+++ public/javascripts/tchat-client.js
@@ -2,7 +2,12 @@ console.log('Ouverture du tchat coté client');
 let socket=io(); 
 
 let formConnection = document.getElementById('form-connection');
+let formMessage = document.getElementById('form-message');
 let inputName = document.getElementById('name');
+let inputMessage = document.getElementById('message');
+let sectionConnexion =document.getElementById('sectionConnexion');
+let sectionMessage =document.getElementById('sectionMessage');
+let divMessages=document.getElementById('messages');
 
 formConnection.addEventListener('submit', (event)=>{
     event.preventDefault();
@@ -11,4 +16,26 @@ formConnection.addEventListener('submit', (event)=>{
 
     //Emission de l'evenement vers le serveur 
     socket.emit('newUser', username);
-})
+
+    //gestion de l'affichage
+sectionConnexion.style.display='none';
+sectionMessage.style.display='block';
+});
+
+formMessage.addEventListener('submit', (event)=>{
+    event.preventDefault();
+    console.log("Envoi d'un message");
+    const message= inputMessage.value;
+
+    //Emission de l'evenement vers le serveur 
+    socket.emit('newMessage', message);
+});
+
+//traitement d'un nouveau message recu (émis par le serveur)
+socket.on('new-message', (username, message)=>{
+    console.log(`Réception d'un message`);
+    //Creation d'un paragraphe HTML
+    const monParagraphe = document.createElement('p');
+    monParagraphe.innerText =`${username} : ${message}`;
+    divMessages.appendChild(monParagraphe);
+});
--- util/socket.js
+++ util/socket.js
@@ -2,11 +2,19 @@
 
 module.exports.tchat=function tchat(io){
     io.on('connection', (socket)=>{
-        console.log('Nouvel utilisateur connecté !')
+        console.log('Nouvel utilisateur connecté !');
+        //Identité de la socket
+        let identity= null;
         // traitement de l'evenement "new user"
         socket.on('newUser',(username)=>{
             console.log("Autentification (coté serveur)");
-            console.log(username);
+            identity = username;
+        })
+        //traitement de l'evenement "newMessage
+        socket.on('newMessage',(message)=>{
+            console.log('${identity} a envoyé un message');
+            //on diffuse le message à tous les utilsateurs
+            io.emit('new-message', identity, message);
         })
     }); 
 }
